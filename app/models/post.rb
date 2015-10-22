f4b0b0fdfcaf7bd77db9dc1dab67ce80cd75a90b
Philip Parker <pparker@noverant.com>
2015-10-22 10:59:11 -0400

    course 2, lesson 4 - extracting slug and vote code to modules

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,51 +1,14 @@
 class Post < ActiveRecord::Base
+  include Voteable
+  include Sluggable
+  
   belongs_to :user, foreign_key: 'user_id', class_name: 'User'
   has_many :comments
   has_and_belongs_to_many :categories
-  has_many :votes, as: :voteable
 
   validates :title, presence: true, length: {minimum: 5}
   validates :url, presence: true
   validates :description, presence: true
 
-  before_save :generate_slug!
-
-  def total_votes
-    self.up_votes - self.down_votes
-  end
-
-  def up_votes
-    self.votes.where(vote: true).size
-  end
-
-  def down_votes
-    self.votes.where(vote: false).size
-  end
-
-  def to_param
-    self.slug
-  end
-
-  def generate_slug!
-    slug_base = to_slug(self.title)
-    slug_current = slug_base
-
-    post = Post.find_by slug: slug_current
-    count = 2
-
-    while post && post != self
-      slug_current = slug_base + "-#{count}"
-      post = Post.find_by slug: slug_current
-      count += 1
-    end
-    self.slug = slug_current
-  end
-
-  def to_slug(name)
-    str = name.strip
-    str.gsub! /\s*[^A-Za-z0-9]\s*/, '-'
-    str.gsub! /-+/, '-'
-    str.downcase
-  end
-    
+  sluggable_column :title    
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+  include Sluggable
+
   has_many :posts
   has_many :comments
   has_many :votes
@@ -8,31 +10,5 @@ class User < ActiveRecord::Base
   validates :username, presence: true, uniqueness: true
   validates :password, presence: true, on: :create, length: {minimum: 5}
 
-  before_save :generate_slug!
-
-  def to_param
-    self.slug
-  end
-
-  def generate_slug!
-    slug_base = to_slug(self.username)
-    slug_current = slug_base
-
-    user = User.find_by slug: slug_current
-    count = 2
-
-    while user && user != self
-      slug_current = slug_base + "-#{count}"
-      user = User.find_by slug: slug_current
-      count += 1
-    end
-    self.slug = slug_current
-  end
-
-  def to_slug(name)
-    str = name.strip
-    str.gsub! /\s*[^A-Za-z0-9]\s*/, '-'
-    str.gsub! /-+/, '-'
-    str.downcase
-  end
+  sluggable_column :username
 end
